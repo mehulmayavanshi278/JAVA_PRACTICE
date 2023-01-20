066635e80a9855fc9e06468878219e4adbe63083
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2023-01-20 18:47:36 +0530

    solved 2nd problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+//    -------------------------------------------
+//    1 Reverse The Array;
+//    int[] arr = new int[8];
+//    System.out.println("enter the Array elements : ");
+//    for(int i=0;i<8;i++){
+//        arr[i] = sc.nextInt();
+//    }
+//    int x=0 , y=7,  mid=8/2 , temp;
+//    while(x<mid || y>mid){
+//     temp=arr[x];
+//     arr[x]=arr[y];
+//     arr[y]=temp;
+//     x++;
+//     y--;
+//    }
+//    for(int i=0;i<8;i++){
+//        System.out.println(arr[i]);
+//    }
+
+
+//    ------------------------------------
+//    2 Find The Maximum and Minimum In The Array
+
+//        int[] arr = new int[5];
+//        System.out.println("Enter the Array  element: ");
+//        for(int i=0;i<5;i++){
+//          arr[i] = sc.nextInt();
+//        }
+//        int min=arr[0] , max=arr[0];
+//        for(int i=1;i<5;i++){
+//
+//            if(min>arr[i]){
+//                min=arr[i];
+//                continue;
+//            }
+//            if(max<arr[i]){
+//                max=arr[i];
+//                continue;
+//            }
+//        }
+//        System.out.println("min is " + min +  "  max is"+ + max);
+
+
     }
 }
