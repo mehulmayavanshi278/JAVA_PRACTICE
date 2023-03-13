7af663068c8a1351b16a42be40732cfd06da3c04
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2023-03-13 21:43:13 +0530

    4th problame solved

--- src/Main.java
+++ src/Main.java
@@ -50,20 +50,56 @@ public class Main {
 //     ----------------------------------------------------
 //     3 Find the Kth minimum And Kth maximum element in Array
 
-        int arr[] = new int[6];
-        System.out.println("Enter the array element");
-        for(int i=0;i<6;i++){
-            arr[i] = sc.nextInt();
+//        int arr[] = new int[6];
+//        System.out.println("Enter the array element");
+//        for(int i=0;i<6;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("enter kth max and kth min choice");
+//        int max = sc.nextInt();
+//        int min = sc.nextInt();
+//        Arrays.sort(arr);
+//        if(max > 6 || max < 0 || min > 6 || min < 0){
+//            System.out.println("Enter Valid Choice");
+//        }else{
+//         System.out.println("the " + max + "th maximum number is" + arr[6-max]);
+//         System.out.println("the " + min + "th minimum number is" + arr[min]);
+//        }
+
+//     4 given an array of only 0 ,1 and 2 sort an array without using any sorting algo.
+            int start=0,mid=0,end=7;
+            System.out.println("enter the array element only in 0 1 and 2");
+            int[] arr = new int[8];
+            int[] arr2 = new int[8];
+            int[] temp = new int[8];
+            for(int i=0;i<8;i++){
+                arr[i] = sc.nextInt();
+            }
+            for(int i=0;i<8;i++){
+                if(arr[i]==0){
+                    arr2[start]=arr[i];
+                    start++;
+                }else if(arr[i]==2){
+                  arr2[end]=arr[i];
+                  end--;
+                }else if(arr[i]==1){
+                    temp[mid]=arr[i];
+                    mid++;
+                }else{
+                    System.out.println("Please Enter Only 0 1 and 2");
+                    break;
+                }
+            }
+        for(int i=0;i<8;i++){
+            if(temp[i]==1){
+                arr2[start]=temp[i];
+                start++;
+            }else{
+                continue;
+            }
         }
-        System.out.println("enter kth max and kth min choice");
-        int max = sc.nextInt();
-        int min = sc.nextInt();
-        Arrays.sort(arr);
-        if(max > 6 || max < 0 || min > 6 || min < 0){
-            System.out.println("Enter Valid Choice");
-        }else{
-         System.out.println("the " + max + "th maximum number is" + arr[6-max]);
-         System.out.println("the " + min + "th minimum number is" + arr[min]);
+        for(int i=0;i<8;i++){
+            System.out.println(arr2[i]);
         }
 
 
