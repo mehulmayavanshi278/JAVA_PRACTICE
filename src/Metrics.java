f318cb793c7eaf5a7a0ce322da3577b0c8f58cf0
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2023-03-27 22:59:55 +0530

    solved 1st metrics

--- src/Metrics.java
+++ src/Metrics.java
@@ -11,114 +11,79 @@ public class Metrics {
             int x = sc.nextInt();
             System.out.println("Enter the column of metrics");
             int y = sc.nextInt();
-            int[][] arr1 = new int[x][y];
+            int[][] a = new int[x][y];
             System.out.println("Enter The Array Element:");
             for(int i=0;i<x;i++){
                 for(int j=0;j<y;j++){
-                    arr1[i][j] = sc.nextInt();
+                    a[i][j] = sc.nextInt();
                 }
             }
 
             for(int i=0;i<x;i++){
                 for(int j=0;j<y;j++){
-                    System.out.print(arr1[i][j] + " ");
+                    System.out.print(a[i][j] + " ");
                 }
                 System.out.println("");
             }
+            int i=0,j=0;
+            boolean up=false , right = true , down = false  ,left = false;
+            int u= -1 , r = y , d = x , l = -1;
 
-            boolean up=false , down=false , left=false;
-            boolean right=true;
-            int u=0,d=x,l=0,r=y,i=0,j=0;
-            int k=-1;
-
-
-
-
-              for(int m=0;m<x*y;m++) {
-
-
-                  if (right) {
-                      System.out.println(" I am moving to the right");
-                      k++;
-                      System.out.print(arr1[i][j]);
-
-                      j++;
-                      System.out.println(" " + i + " " + j + "     " + " k is " + k);
-                      if (j == r) {
-                          right = false;
-                          left = false;
-                          up = false;
-                          down = true;
-                          u++;
-                          i++;
-                          j--;
-                         continue;
-
-                      }
-                  }
-
-
-                 else if (down) {
-                      System.out.println(" I am moving to the down");
-                      k++;
-                      System.out.print(arr1[i][j]);
-                      i++;
-                      System.out.println(" " + i + " " + j + "     " + " k is " + k);
-                      if (i == d) {
-                          right = false;
-                          left = true;
-                          up = false;
-                          down = false;
-                          r--;
-                          j--;
-                          i--;
-                          continue;
-
-                      }
-
-                  }
-
-
-                 else if (left) {
-                      System.out.println(" I am moving to the left");
-                      k++;
-                      System.out.print(arr1[i][j]);
-                      j--;
-                      System.out.println(" " + i + " " + j + "     " + " k is " + k);
-                      if (j == l) {
-                          right = false;
-                          left = false;
-                          up = true;
-                          down = false;
-                          d--;
-//                            i--;
-                          j++;
-                          continue;
-
-                      }
-
-                  }
-
-                 else if (up) {
-                      System.out.println(" I am moving to the up");
-                      k++;
-                      System.out.print(arr1[i][j]);
-                      i--;
-                      System.out.println(" " + i + " " + j + "     " + " k is " + k);
-                      if (i == u) {
-                          right = true;
-                          left = false;
-                          up = false;
-                          down = false;
-                          l++;
-                          continue;
-
-                      }
-
-
-                  }
-
-              }
+            for(int m=0;m<x*y;m++){
+                if(right){
+                    System.out.print(a[i][j] + " ");
+                    j++;
+                    if(j==r){
+                        j--;
+                        i++;
+                        right=false;
+                        down=true;
+                        left=false;
+                        up=false;
+                        u++;
+                        continue;
+                    }
+                }else if(down){
+                    System.out.print(a[i][j] + " ");
+                    i++;
+                    if(i==d){
+                        i--;
+                        j--;
+                        r--;
+                        left=true;
+                        down=false;
+                        right=false;
+                        up=false;
+                        continue;
+                    }
+                }else if(left){
+                    System.out.print(a[i][j] + " ");
+                    j--;
+                    if(j==l){
+                        j++;
+                        i--;
+                        d--;
+                        up=true;
+                        right=false;
+                        down=false;
+                        left=false;
+                        continue;
+                    }
+                }else if(up){
+                    System.out.print(a[i][j] + " ");
+                    i--;
+                    if(i==u){
+                        i++;
+                        j++;
+                        l++;
+                        left=false;
+                        up=false;
+                        down=false;
+                        right=true;
+                        continue;
+                    }
+                }
+            }
 
 
 
