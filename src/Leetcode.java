263184aadb86c86282b0c3c3d22fba03fc22c406
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2023-11-06 22:53:52 +0530

    leetcode MergeTwoSortedArray Solved

--- src/Leetcode.java
+++ src/Leetcode.java
@@ -1,10 +1,18 @@
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.*;
+import java.lang.String;
 
 public class Leetcode {
 
 
-
+     public static void PrintArray(int[] a){
+         System.out.print("Array Element is");
+       for(int i=0;i<a.length;i++){
+           System.out.print( " " + a[i] + " ");
+       }
+     }
 
     public int romanToInt(String s1) {
 
@@ -79,6 +87,184 @@ public class Leetcode {
         return isPalindrm;
     }
 
+//    public static String longestCommonprefex(String[] s){        // using sorting technique
+//        String result="";
+//        Arrays.sort(s);
+//        int i=0;
+//        while(s[0].charAt(i) == s[s.length-1].charAt(i)){
+//            result = result+s[s.length-1].charAt(i);
+//            i++;
+//        }
+//        return result;
+//    }
+
+    public static String longestCommonprefex(String[] strs){
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        System.out.println(minLength);
+        // Find the minimum length of the strings in the array
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+            System.out.println(minLength);
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < minLength; i++) {
+            char currentChar = strs[0].charAt(i);
+
+            // Compare the current character with the same position in other strings
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != currentChar) {
+                    return result.toString(); // Found a mismatch, return the common prefix found so far
+                }
+            }
+
+            // If all characters match, append the character to the result
+            result.append(currentChar);
+        }
+
+        return result.toString();
+    }
+
+//     public  HashMap<Integer , Integer>  getBestProfitableStock(int maxPrice , int[] a , int[] b){
+//        HashMap<Integer , Integer> MAP = new HashMap<>();
+//        int[] c = new int[a.length];
+//          for(int i=0;i<a.length;i++){
+//              MAP.put(b[i] - a[i] , a[i]);
+//              c[i]=b[i] - a[i];
+//          }
+//          Arrays.sort(c);
+//          System.out.println(MAP);
+//
+//          for(int i=0;i<c.length;i++){
+//              System.out.println(c[i]);
+//          }
+//          HashMap<Integer , Integer> result = new HashMap<>();
+//          int remainingPrice = maxPrice;
+//          for(int i=c.length-1;i>=0;i--){
+//              int noOfStock=0;
+//              if(remainingPrice>0){
+//                  noOfStock = remainingPrice/MAP.get(c[i]);
+//                  System.out.println("before remainin Price is" + remainingPrice);
+//                  remainingPrice = remainingPrice - ((remainingPrice/MAP.get(c[i])) * MAP.get(c[i]));
+//                  System.out.println("after remainin Price is" + remainingPrice);
+//                  System.out.println("remaining price for" + MAP.get(c[i]) + " is " + remainingPrice);
+//                  result.put(MAP.get(c[i]) , noOfStock);
+//              }
+//          }
+//        return result;
+//
+//    }
+
+     public HashMap<Integer , Integer> getBestProfitableStock(int maxPrice , int[] a , int[] b){
+        HashMap<Integer , Integer> MAP = new HashMap<>();
+        HashMap<Integer , Integer> result = new HashMap<>();
+        int[] c = new int[a.length];
+        for(int i=0;i<a.length;i++){
+            MAP.put(b[i] - a[i] , a[i]);
+            c[i] = b[i] - a[i];
+        }
+        Arrays.sort(c);
+        for(int i=0;i<c.length;i++){
+            System.out.println(c[i]);
+        }
+        int noOfStock = 0;
+        int RemainingPrice = maxPrice;
+        for(int i=c.length-1;i>=0;i--){
+           if(RemainingPrice>0){
+               noOfStock = RemainingPrice/MAP.get(c[i]);
+               RemainingPrice = RemainingPrice - (RemainingPrice/MAP.get(c[i]) * MAP.get(c[i]));
+               result.put(MAP.get(c[i]) , noOfStock);
+           }
+        }
+        return result;
+     }
+
+     public boolean CheckValidParanthesis(String s1){  // ({[]}{}]])
+        if(s1.length()==0){
+            return false;
+        }
+        Boolean isValid = true;
+        Stack<Character> STACK = new Stack<>();
+        int CurrIndexOfS1=0;
+        while(CurrIndexOfS1<s1.length()){
+            Character currentChar = s1.charAt(CurrIndexOfS1);
+            if(currentChar == '(' || currentChar == '[' || currentChar == '{'){
+                System.out.println("yes");
+                STACK.push(currentChar);
+                CurrIndexOfS1++;
+                System.out.println(CurrIndexOfS1);
+//                System.out.println("current character is" + currentChar);
+            } else if (currentChar==')' || currentChar==']' || currentChar=='}') {
+                System.out.println(CurrIndexOfS1);
+                System.out.println("current character is" + currentChar);
+                System.out.println("top element is" + STACK.peek());
+               if (currentChar==')' && STACK.peek() == '('){
+                  STACK.pop();
+                  CurrIndexOfS1++;
+               } else if (currentChar==']' && STACK.peek() == '['){
+                    STACK.pop();
+                    CurrIndexOfS1++;
+               }else if (currentChar=='}' && STACK.peek() == '{'){
+                  STACK.pop();
+                  CurrIndexOfS1++;
+               }else{
+                  System.out.println("not valid");
+                  return false;
+               }
+            }else{
+                System.out.println("current character is" + currentChar);
+                break;
+            }
+        }
+
+        if(STACK.isEmpty()){
+            return isValid;
+        }else{
+            return !isValid;
+        }
+
+     }
+
+     public int[] mergeTwoSortedArray(int[] a , int[] b){
+         int[] result = new int[a.length + b.length];
+         int i=0 ,j=0;
+         int k=0;
+         while(i<a.length && j<b.length){
+             if(a[i]==b[j]){
+                 result[k]=a[i];
+                 result[++k] = a[i];
+                 i++;
+                 j++;
+                 k++;
+             } else if (a[i]<b[j]) {
+                 result[k] = a[i];
+                 i++;
+                 k++;
+             }else {
+                 result[k] = b[j];
+                 j++;
+                 k++;
+             }
+         }
+         while(i<a.length){
+             result[k]=a[i];
+             i++;
+             k++;
+         }
+         while(j<b.length){
+             result[k]=b[j];
+             j++;
+             k++;
+         }
+
+         return result;
+     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -107,13 +293,40 @@ public class Leetcode {
 //        System.out.println(output);
 
 //        4  palindrome
-        System.out.println("Enter the number to test palindrome");
-        int num = sc.nextInt();
-        Leetcode L1 = new Leetcode();
-       boolean result = L1.isPalindrome(num);
-        System.out.println(result);
+//        System.out.println("Enter the number to test palindrome");
+//        int num = sc.nextInt();
+//        Leetcode L1 = new Leetcode();
+//       boolean result = L1.isPalindrome(num);
+//        System.out.println(result);
+
+//        5 longest common prefix
+//
+//        String[] s1 = {  "interstellar" , "interract" ,  "in"};
+//        String result = longestCommonprefex(s1);
+//        System.out.println(result + " is common in all elements");
+
+//        6  stock problame
+//          int[] buyPrice = {10 , 8 , 11 , 4};
+//          int[] futurePrice = {7 , 8 , 13 , 5};
+//          int maxPrice = 350;
+//          HashMap<Integer , Integer> HMAP = new HashMap<>();
+//          Leetcode L1 = new Leetcode();
+//          HMAP =L1.getBestProfitableStock(maxPrice , buyPrice , futurePrice);
+//          System.out.println(HMAP);
+
+//        7   check valid paranthesis
+//           String s1 = "({}[]()()({})]";
+//           Leetcode L1 = new Leetcode();
+//           Boolean isValid = L1.CheckValidParanthesis(s1);
+//           System.out.println(isValid);
 
 
+//        8  merge two sorted array
+             int[] a = {1 , 5 , 7 ,9};
+             int[] b = {1 , 3 , 5 ,14};
+             Leetcode L1 = new Leetcode();
+             int[] result = L1.mergeTwoSortedArray(a ,b);
+             PrintArray(result);
 
     }
 }
